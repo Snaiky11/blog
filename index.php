7cc4dccb21506b875760ba0597e6dd68ef1f40b1
Snaiky11 <30751506+Snaiky11@users.noreply.github.com>
2017-09-27 20:52:50 +0200

    Install bootstrap calendar

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
         <script type="text/javascript" src="view/js/jquery-3.2.1.js"></script>
         <script type="text/javascript" src="view/js/bootstrap.js"></script>
         <script type="text/javascript" src="view/js/npm.js"></script>
+        <script type="text/javascript" src="view/js/moment.js"></script>
+        <script type="text/javascript" src="view/js/bootstrap-datetimepicker.js"></script>
     </head>
     <body>
         <div class="container-fluid" id="backpage">
@@ -27,7 +29,7 @@
                         <div class="row">
                             <div class="col-md-6">
                                 <div class="thumbnail">
-                                    <img src="view\css\img\sina.png" alt="...">
+                                    <img src="view\css\img\background5.jpg" alt="...">
                                     <div class="caption">
                                         <h3>Thumbnail label</h3>
                                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -37,7 +39,7 @@
                             </div>
                             <div class="col-md-6">
                                 <div class="thumbnail">
-                                    <img src="view\css\img\sina.png" alt="...">
+                                    <img src="view\css\img\gaming-wallpapers-25.jpg" alt="...">
                                     <div class="caption">
                                         <h3>Thumbnail label</h3>
                                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
--- view/header.php
+++ view/header.php
@@ -27,7 +27,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">Brand</a>
+                <a class="navbar-brand" href="#"><h3 class="logo">SINA<small class="logo">.fr</small></h3></a>
             </div> <!-- navbar-header -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
